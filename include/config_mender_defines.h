c44d0a723205e7ec75e45783c835b6cce1ea1af2
Drew Moseley <drew.moseley@northern.tech>
2019-09-20 12:53:52 -0400

    bbb-mender: Add separate defconfig for emmc based system.
    This allows a single branch to be used reducing maintenance issues.
    
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- include/config_mender_defines.h
+++ include/config_mender_defines.h
@@ -12,15 +12,24 @@
 #define MENDER_ROOTFS_PART_B_NUMBER 3
 #define MENDER_ROOTFS_PART_B_NUMBER_HEX 3
 #define MENDER_UBOOT_STORAGE_INTERFACE "mmc"
-#define MENDER_UBOOT_STORAGE_DEVICE 0
+
+#ifdef CONFIG_ROOTFS_EMMC
+#  define MENDER_UBOOT_STORAGE_DEVICE 1
+#  define MENDER_STORAGE_DEVICE "/dev/mmcblk1"
+#  define MENDER_STORAGE_DEVICE_BASE "/dev/mmcblk1p"
+#  define MENDER_ROOTFS_PART_A_NAME "/dev/mmcblk1p2"
+#  define MENDER_ROOTFS_PART_B_NAME "/dev/mmcblk1p3"
+#else
+#  define MENDER_UBOOT_STORAGE_DEVICE 0
+#  define MENDER_STORAGE_DEVICE "/dev/mmcblk0"
+#  define MENDER_STORAGE_DEVICE_BASE "/dev/mmcblk0p"
+#  define MENDER_ROOTFS_PART_A_NAME "/dev/mmcblk0p2"
+#  define MENDER_ROOTFS_PART_B_NAME "/dev/mmcblk0p3"
+#endif
 
 /* BB variables. */
-#define MENDER_STORAGE_DEVICE "/dev/mmcblk0"
-#define MENDER_STORAGE_DEVICE_BASE "/dev/mmcblk0p"
 #define MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_1 0x800000
 #define MENDER_UBOOT_ENV_STORAGE_DEVICE_OFFSET_2 0x1000000
-#define MENDER_ROOTFS_PART_A_NAME "/dev/mmcblk0p2"
-#define MENDER_ROOTFS_PART_B_NAME "/dev/mmcblk0p3"
 
 /* For sanity checks. */
 #define MENDER_BOOTENV_SIZE 0x20000
